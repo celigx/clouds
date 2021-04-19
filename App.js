b527eba59d521f9e93ee7d6c56c62471c3cb1274
Celig <cela@protonmail.com>
2021-04-19 11:26:11 +0200

    Add API , Context
    Create WeatherContext to pass data through the component tree
    
    Create fetchWeather to fetch weather data from OpenWeatherMap

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { WEATHER_API_KEY } from '@env';
+
+import WeatherContext from './context/WeatherContext';
 
 export default function App() {
+  const [weather, setWeather] = useState({
+    city:'Zagreb',
+    unit:'metric',
+    temp:'',
+    conditions:'',
+    wind:'',
+    humidity:'',
+    sunrise:'',
+    sunset:'',
+    lat:'45.8153',
+    log:'15.9665',
+    daily:'',
+    hourly:'',
+    isLoading: true,
+  })
+
+  useEffect(() => {
+    fetchWeather()
+  }, [weather.city, weather.lat, weather.log])
+
+  const fetchWeather = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${weather.lat}&lon=${weather.log}&appid=${WEATHER_API_KEY}&units=${weather.unit}`)
+      .then(response => response.json())
+      .then(data => {
+        setWeather((prevState) => ({
+          ...prevState,
+          temp: data.current.temp,
+          conditions: data.current.weather[0].main,
+          wind: data.current.wind_speed,
+          humidity: data.current.humidity,
+          sunrise: data.current.sunrise,
+          sunset: data.current.sunset,
+          daily: data.daily,
+          hourly: data.hourly,
+          isLoading:false
+        }))    
+      })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <WeatherContext.Provider value={{ weather, setWeather, fetchWeather }}>
       <StatusBar style="auto" />
-    </View>
+    </WeatherContext.Provider>
   );
 }
 
